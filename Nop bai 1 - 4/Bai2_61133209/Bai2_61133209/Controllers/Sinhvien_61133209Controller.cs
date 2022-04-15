f3ed9c4a7290617c5b9964364214b8bcf90f3872
HuynhNhatTruong0108 <kuda.2001.08.01@gmail.com>
2022-04-15 10:04:18 +0700

    baitap

---
+++ Nop bai 1 - 4/Bai2_61133209/Bai2_61133209/Controllers/Sinhvien_61133209Controller.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bai2_61133209.Controllers
+{
+    public class Sinhvien_61133209Controller : Controller
+    {
+        // GET: Sinhvien_61133209
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Index2(FormCollection field)
+        {
+            ViewBag.Id = field["Id"];
+            ViewBag.Name = field["Name"];
+            ViewBag.Marks = field["Marks"];
+            return View(ViewBag);
+
+        }
+    }
+}
---
+++ Nop bai 1 - 4/bai4_61133209/bai4_61133209/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="bai4_61133209.MvcApplication" Language="C#" %>
