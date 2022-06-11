bbe3f91c289de7c1d3d97f14bbd5278ec65847d1
HuynhNhatTruong0108 <kuda.2001.08.01@gmail.com>
2022-06-11 20:06:44 +0700

    bai tap 6

---
+++ Bai6_61133209/Bai6_61133209/Controllers/GioiThieu_61133209Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bai6_61133209.Controllers
+{
+    public class GioiThieu_61133209Controller : Controller
+    {
+        // GET: GioiThieu_61133209
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
